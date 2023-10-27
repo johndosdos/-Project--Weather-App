c4a4f1a4781662cdf8bbedf6133392622ea3fffd
John <128350391+alpaOo@users.noreply.github.com>
2023-10-28 02:49:11 +0800

    New: 7 day forecast with high and low temperature
    modified:   public/index.html
    modified:   src/index.html
      - Minor changes. The table is dynamically created using JS
    
    modified:   public/main.js
    modified:   src/index.js
      - Update the value for forecastDays to fetch forecast data from
        WeatherAPI, 7 day forecast
      - Create table dynamically and show proper data for each day
        of the week with high and low temperature
      - Remove existing data before appending the newly fetched data from
        the API
    
    modified:   src/style.css
      - Add minimal syling to the page
    
    there's so much to learn. i'm getting there

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import css from "./style.css";
 
 function createWeatherApp() {
-  const { location, temperature, search, condition, minmax, loading } = init();
+  //initialize global variables
+  const {
+    location,
+    temperature,
+    search,
+    condition,
+    minmax,
+    loading,
+    tbody_forecast,
+    table_forecast,
+  } = init();
 
   function init() {
+    const search = document.querySelector("#search"); //search bar
+    //main weather display
     const location = document.querySelector("#location");
     const temperature = document.querySelector("#temperature");
-    const search = document.querySelector("#search");
     const condition = document.querySelector("#condition");
     const minmax = document.querySelector("#minmax");
-    const loading = document.querySelector(".loading");
-
-    search.addEventListener("keypress", function (e) {
-      if (e.code === "Enter") {
-        getWeatherData();
-      }
-    });
+    //end
+    const loading = document.querySelector(".loading"); //loading animation
+    const tbody_forecast = document.querySelector(
+      "#dayForecast > table > tbody"
+    );
+    const table_forecast = document.querySelector("#dayForecast > table");
 
     return {
       location,
@@ -24,8 +34,21 @@ function createWeatherApp() {
       condition,
       minmax,
       loading,
+      tbody_forecast,
+      table_forecast,
     };
   }
+  //end
+
+  //search city to get weather data from
+  function searchCity() {
+    search.addEventListener("keypress", function (e) {
+      if (e.code === "Enter") {
+        getWeatherData();
+      }
+    });
+  }
+  //end
 
   //set a loading progress when fetching data
   function loadingTrue() {
@@ -35,21 +58,12 @@ function createWeatherApp() {
   function loadingFalse() {
     loading.setAttribute("aria-busy", false);
   }
+  //end
 
-  function toProcessData(data) {
-    const weatherData = data;
-
-    location.textContent = `${data.location.name},\n${data.location.country}`;
-    temperature.textContent = `${data.current.temp_c}℃`;
-    condition.textContent = `${data.forecast.forecastday[0].day.condition.text}`;
-    minmax.textContent = `H: ${data.forecast.forecastday[0].day.maxtemp_c}℃\u00A0\u00A0\u00A0\u00A0L: ${data.forecast.forecastday[0].day.mintemp_c}℃`;
-
-    console.log(weatherData);
-  }
-
+  //fetch data from WeatherAPI
   async function getWeatherData() {
     const searchQuery = search.value;
-    const forecastDays = 1;
+    const forecastDays = 8;
     const apiKey = "05769b947394471b9cb145605231710";
     const weatherAPI = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${searchQuery}&days=${forecastDays}&aqi=no&alerts=no`;
 
@@ -61,6 +75,7 @@ function createWeatherApp() {
       if (response.ok === true) {
         const data = await response.json();
         toProcessData(data);
+        toProcessForecastData(data);
       }
     } catch (error) {
       console.log(error);
@@ -68,5 +83,67 @@ function createWeatherApp() {
       loadingFalse();
     }
   }
+  //end
+
+  //process the promised JSON file, display to browser
+  function toProcessData(JSONdata) {
+    const weatherData = JSONdata;
+
+    location.textContent = `${JSONdata.location.name},\n${JSONdata.location.country}`;
+    temperature.textContent = `${JSONdata.current.temp_c}℃`;
+    condition.textContent = `${JSONdata.forecast.forecastday[0].day.condition.text}`;
+    minmax.textContent = `H: ${JSONdata.forecast.forecastday[0].day.maxtemp_c}℃\u00A0\u00A0\u00A0\u00A0L: ${JSONdata.forecast.forecastday[0].day.mintemp_c}℃`;
+
+    console.log(weatherData);
+  }
+  //end
+
+  //process forecast data
+  function toProcessForecastData(JSONdata) {
+    //check if <caption> has been created or not
+    if (
+      table_forecast.querySelector("#dayForecast > table > caption") === null
+    ) {
+      const caption = document.createElement("caption");
+      caption.textContent = "7-day forecast";
+      table_forecast.appendChild(caption);
+    }
+    //end
+
+    //remove the table data when searching for a new location
+    while (tbody_forecast.firstChild) {
+      tbody_forecast.removeChild(tbody_forecast.firstChild);
+    }
+    //end
+
+    const forecastArray = JSONdata.forecast.forecastday;
+
+    forecastArray.forEach(function (day, index) {
+      const tr = document.createElement("tr");
+      const td_day = document.createElement("td");
+      const td_tempHI = document.createElement("td");
+      const td_tempLo = document.createElement("td");
+
+      const date = new Date(day.date);
+      const daysOfTheWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+      if (index === 0) {
+        td_day.textContent = "Today";
+      } else if (index === 1) {
+        td_day.textContent = "Tomorrow";
+      } else {
+        td_day.textContent = daysOfTheWeek[date.getDay()];
+      }
+      td_tempHI.textContent = `H: ${day.day.maxtemp_c}℃`;
+      td_tempLo.textContent = `L: ${day.day.mintemp_c}℃`;
+      tr.appendChild(td_day);
+      tr.appendChild(td_tempHI);
+      tr.appendChild(td_tempLo);
+      tbody_forecast.appendChild(tr);
+    });
+  }
+  //end
+
+  searchCity();
 }
 createWeatherApp();
